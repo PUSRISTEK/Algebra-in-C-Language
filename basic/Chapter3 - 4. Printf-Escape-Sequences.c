1f3fa65aa02fa4f4fdbd48c7c862c8fecdadafab
Techno Camp <gungunfebrianza@gmail.com>
2016-02-07 17:55:12 +0700

    Create Chapter3 - 4. Printf-Escape-Sequences.c

---
+++ basic/Chapter3 - 4. Printf-Escape-Sequences.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+printf("\"Barangsiapa tidak mau merasakan pahitnya belajar, Ia akan merasakan hinanya kebodohan sepanjang hidupnya.\"\n");
+printf("- Imam Syafi'i\n");
+}
